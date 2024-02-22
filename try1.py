64e4e01f1451a3ec7013ad9883f6b3d3e8765f97
Yitkhee <135970016+yitkhee17@users.noreply.github.com>
2024-02-22 16:57:40 +0800

    Update try1.py

--- try1.py
+++ try1.py
@@ -1,11 +1,10 @@
 # Import necessary libraries
 import numpy as np
-from keras.models import Model
-from keras.layers import Input, LSTM, Dense
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-from keras.layers import Embedding
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Dense, Embedding
+from tensorflow.keras.utils import to_categorical
 
 # Load data from file
 def load_data(file_path):
@@ -13,74 +12,65 @@ def load_data(file_path):
         data = f.read().split('\n')
     return data
 
-# Tokenize the text
-def tokenize_text(text):
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(text)
-    return tokenizer
+source_sentences = load_data('source.txt')
+target_sentences = load_data('target.txt')
 
-# Convert text to number (word embedding)
-def text_to_sequences(tokenizer, text):
-    sequences = tokenizer.texts_to_sequences(text)
-    return sequences
+# Tokenize the text (convert text to sequences of integers)
+tokenizer_source = Tokenizer()
+tokenizer_source.fit_on_texts(source_sentences)
+source_sequences = tokenizer_source.texts_to_sequences(source_sentences)
+
+tokenizer_target = Tokenizer()
+tokenizer_target.fit_on_texts(target_sentences)
+target_sequences = tokenizer_target.texts_to_sequences(target_sentences)
 
 # Word to index mapping
-def word_to_index(tokenizer):
-    word_index = tokenizer.word_index
-    return word_index
+source_index_word = tokenizer_source.index_word
+target_index_word = tokenizer_target.index_word
 
-# Load pretrained word embeddings (GloVe)
-def load_glove_embeddings(file_path):
-    embeddings_index = {}
-    with open(file_path) as f:
-        for line in f:
-            values = line.split()
-            word = values[0]
-            coefs = np.asarray(values[1:], dtype='float32')
-            embeddings_index[word] = coefs
-    return embeddings_index
+# Determine max length for padding sequences
+max_source_length = max([len(seq) for seq in source_sequences])
+max_target_length = max([len(seq) for seq in target_sequences])
+
+# Pad sequences to max length
+source_data = pad_sequences(source_sequences, maxlen=max_source_length)
+target_data = pad_sequences(target_sequences, maxlen=max_target_length)
+
+# Load pre-trained word embeddings (GloVe)
+embeddings_index = {}
+with open('glove.6B.100d.txt', 'r') as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
 
 # Prepare input embedding matrix
-def prepare_embedding_matrix(word_index, embeddings_index):
-    embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
-    for word, i in word_index.items():
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-            embedding_matrix[i] = embedding_vector
-    return embedding_matrix
+embedding_dim = 100  # or whatever dimension GloVe you are using
+source_vocab_size = len(tokenizer_source.word_index) + 1
+embedding_matrix = np.zeros((source_vocab_size, embedding_dim))
+for word, i in tokenizer_source.word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
 
-# One-hot-target
-def one_hot_target(sequences, num_classes):
-    targets = to_categorical(sequences, num_classes=num_classes)
-    return targets
+# One-hot encode targets
+target_data_one_hot = to_categorical(target_data)
 
-# Model building process
-def build_model(input_dim, output_dim, hidden_units):
-    encoder_inputs = Input(shape=(None, input_dim))
-    encoder = LSTM(hidden_units, return_state=True)
-    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-    encoder_states = [state_h, state_c]
+# Define model
+encoder_inputs = Input(shape=(None,))
+encoder_embedding = Embedding(source_vocab_size, embedding_dim, weights=[embedding_matrix], trainable=False)(encoder_inputs)
+encoder_lstm = LSTM(256, return_state=True)
+encoder_outputs, state_h, state_c = encoder_lstm(encoder_embedding)
+encoder_states = [state_h, state_c]
 
-    decoder_inputs = Input(shape=(None, output_dim))
-    decoder_lstm = LSTM(hidden_units, return_sequences=True, return_state=True)
-    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-    decoder_dense = Dense(output_dim, activation='softmax')
-    decoder_outputs = decoder_dense(decoder_outputs)
+decoder_inputs = Input(shape=(None,))
+decoder_embedding = Embedding(target_vocab_size, embedding_dim)
+decoder_lstm = LSTM(256, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_embedding(decoder_inputs), initial_state=encoder_states)
+decoder_dense = Dense(target_vocab_size, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
 
-    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-    return model
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
-# Compiling
-def compile_model(model):
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    return model
-
-# Training
-def train_model(model, encoder_input_data, decoder_input_data, decoder_target_data, epochs, batch_size):
-    model.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-    return model
-
-# Testing
-def test_model(model, test_data):
-    predictions = model.predict(test_data)
-    return predictions
+# Compile
